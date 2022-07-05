b3a8ddd16cc96cadf8ba9aab7887430ee4e40b88
KhunBall <indeeprw61@gmail.com>
2022-07-05 22:03:54 +0700

    Add navigation with routing

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -1,16 +1,27 @@
-import { TestBed } from '@angular/core/testing';
+import { Injectable } from '@angular/core';
 
-import { HeroService } from './hero.service';
+import { Observable, of } from 'rxjs';
 
-describe('HeroService', () => {
-  let service: HeroService;
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(HeroService);
-  });
+@Injectable({ providedIn: 'root' })
+export class HeroService {
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // For now, assume that a hero with the specified `id` always exists.
+    // Error handling will be added in the next step of the tutorial.
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
+}
